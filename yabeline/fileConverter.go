247ba49b5b6a04bbafea43637fdaf9c8389ba544
thearturca <arti.1046@gmail.com>
2025-05-29 14:32:01 +0300

    feat(image-converter): keep original height of image

--- yabeline/fileConverter.go
+++ yabeline/fileConverter.go
@@ -18,7 +18,7 @@ func ConvertImage(image []byte) ([]byte, error) {
 		// Filter("scale", ffmpeg.Args{"min'(512,iw)':min'(512,ih)"}).
 		Output("pipe:", ffmpeg.KwArgs{
 			"c:v":      "png",
-			"vf":       "scale=512:-1",
+			"vf":       "scale=512:min'(512,ih)':force_original_aspect_ratio=decrease",
 			"frames:v": "1",
 			"pix_fmt":  "rgba",
 			"f":        "image2",
